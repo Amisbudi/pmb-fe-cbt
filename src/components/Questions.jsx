24c89d7dab693e61fd7065a3f55c49e30e061f93
Lerian Febriana <kanglerian@gmail.com>
2024-09-25 08:07:34 +0700

    download excel template

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,6 +4,7 @@ import { faEdit, faKey, faPlusCircle, faSave, faTrashAlt, faUpload, faXmark, faX
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
 import axios from 'axios'
 import LoadingScreen from './LoadingScreen'
+import ExampleImportQuestion from '../assets/example-import-questions.xlsx'
 
 const Questions = () => {
   const [createModal, setCreateModal] = useState(false);
@@ -691,6 +692,11 @@ const Questions = () => {
                     <div className="col-span-2">
                       <label htmlFor="excel" className="block mb-2 text-sm font-medium text-gray-900">Excel</label>
                       <input type='file' name="excel" id="excel" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" />
+                    </div>
+                    <div className="col-span-2">
+                      <a href={ExampleImportQuestion} className='text-xs underline' download="example-import-questions.xlsx">
+                        Download Example Template
+                      </a>
                     </div>
                   </div>
                   <button type="submit" className="text-white inline-flex items-center bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl space-x-2 text-sm px-5 py-2.5 text-center">
